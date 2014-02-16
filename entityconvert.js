fc2e769a62faa075800046e56ecf8b81c7e273f4
m90 <frederik.ring@gmail.com>
2014-02-16 16:22:30 +0100

    fix package.json

--- entityconvert.js
+++ entityconvert.js
@@ -1,10 +1,10 @@
-(function (root, factory) {
+(function(root, factory){
 
-	if (typeof define === 'function' && define.amd) {
+	if (typeof define === 'function' && define.amd){
 		define(function(){
 			return factory();
 		});
-	} else if (typeof exports === 'object') {
+	} else if (typeof exports === 'object'){
 		module.exports = factory();
 	} else {
 		root.entityconvert = factory();
