9d7c2600d4c348ab79dd35248146c4c12fec2ea5
m90 <frederik.ring@gmail.com>
2014-02-16 13:47:53 +0100

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# entityconvert.js
+convert special characters in a string to their HTML or CSS charcode entities (useful for build tools et. al.)
+
+The module works as AMD or CommonJS module and exports an Object containing to functions: `.html(str)` and `.css(str)`. In a non-AMD or CommonJS environment `window.entityconvert` will be exported.
+
+AMD usage like:
+```javascript
+require(['entities'], function(entityconvert){
+	var converted = entityconvert.html('We äll löve Ümläutß!');
+	// => We &#228;ll l&#246;ve &#220;ml&#228;ut&#223;!
+});
+```
+
+CommonJS usage like:
+var entityconvert = require('entityconvert');
+var converted = entityconvert.css('We äll löve Ümläutß!');
+// => We \00e4ll l\00f6ve \00dcml\00e4ut\00df!
+
+Available via npm:
+```sh
+npm install entity-convert --save
+```
+
+##License
+MIT © [Frederik Ring](https://github.com/m90)
---
+++ entityconvert.js
@@ -0,0 +1,40 @@
+function replacer(mode){
+	return function(char){
+		var index = char.charCodeAt(0);
+		if (index > 127){
+			return mode === 'css' ? ('\\00' + char.charCodeAt(0).toString(16)) : ('&#' + char.charCodeAt(0) + ';');
+		} else {
+			return char;
+		}
+	};
+}
+
+function replaceBy(mode){
+
+	return function(string){
+		var fn = replacer(mode);
+		string = string.split('');
+		for (var i = 0, len = string.length; i < len; i++){
+			string[i] = fn(string[i]);
+		}
+		return string.join('');
+	};
+
+}
+
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(function(){
+			return factory();
+		});
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	} else {
+		root.entityconvert = factory();
+	}
+}(this, function(){
+	return {
+		css : replaceBy('css')
+		, html : replaceBy('html')
+	};
+}));
